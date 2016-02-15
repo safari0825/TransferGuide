d0764d7d517e353561739c528a6b55845bcf1c15
safari0825 <liujin0425@outlook.com>
2016-02-15 16:37:39 +0900

    add & delete via OK

--- app/src/main/java/jp/co/pp/transferguide/MainActivity.java
+++ app/src/main/java/jp/co/pp/transferguide/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.AttributeSet;
@@ -25,13 +26,18 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     protected int viaCount = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+        for (int i = 0; i < 3; i++)
+            tabLayout.addTab(tabLayout.newTab().setText("选项卡" + i));
+        tabLayout.setTabMode(TabLayout.MODE_FIXED);
 
         final Button srchButton = (Button) findViewById(R.id.srchButton);
         final EditText text1 = (EditText)findViewById(R.id.startInput);
