5eff31e2eb29c44675cc001b344cb7bce1b57e55
safari0825 <liujin0425@outlook.com>
2016-02-15 16:37:23 +0900

    add & delete via OK

--- app/src/main/java/jp/co/pp/transferguide/SrchResultActivity.java
+++ app/src/main/java/jp/co/pp/transferguide/SrchResultActivity.java
@@ -3,12 +3,14 @@ package jp.co.pp.transferguide;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.TableLayout;
 
 public class SrchResultActivity extends AppCompatActivity {
 
@@ -17,7 +19,19 @@ public class SrchResultActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_srch_result);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setNavigationIcon(R.mipmap.ic_launcher);
         setSupportActionBar(toolbar);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+
+        for (int i = 0; i < 3; i++)
+//        {
+//
+//            TabLayout.Tab tab1 = (TabLayout.Tab) tabLayoutBtm.newTab();
+//            tab1.setText("选项卡" + i);
+//        }
+        tabLayout.addTab(tabLayout.newTab().setText("选项卡" + i));
+        tabLayout.setTabMode(TabLayout.MODE_FIXED);
 
         final RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
 
